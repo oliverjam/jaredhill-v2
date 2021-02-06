59e4e1090063ea9d3bc627f9b01bd6f6514bedcc
oliverjam <oliverjamesphillips@gmail.com>
2021-02-06 19:55:10 +0000

    Fix nav ordering

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -3,7 +3,11 @@ module.exports = {
     let nav = [];
     for (let page of data.collections.all) {
       if (page.data.nav) {
-        nav.push({ label: page.data.nav.label, url: page.url });
+        nav.push({
+          label: page.data.nav.label,
+          order: page.data.nav.order,
+          url: page.url,
+        });
       }
     }
     return nav.sort((a, b) => a.order - b.order);
