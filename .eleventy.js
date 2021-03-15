5719982c995ad8a95d8be40b93b9056334cc202b
oliverjam <oliverjamesphillips@gmail.com>
2021-03-15 22:12:57 +0000

    Add favicon from old site
    Closes #16

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = (config) => {
   config.addWatchTarget("src/css");
+  config.addPassthroughCopy({ "src/assets/favicon.ico": "favicon.ico" });
   config.addPassthroughCopy("src/assets");
   config.addPassthroughCopy("src/work/**/*.{png,jpg,gif}");
   // merges directory-level data with template-specific data when keys clash
