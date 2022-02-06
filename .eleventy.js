d84de35ec4b387feaa722a566d1b0938b995b255
oliverjam <oliverjamesphillips@gmail.com>
2022-02-06 12:10:04 +0000

    Upgrade dependencies to fix the build

--- .eleventy.js
+++ .eleventy.js
@@ -10,11 +10,8 @@ module.exports = (config) => {
   config.addPassthroughCopy({ "src/assets/favicon.ico": "favicon.ico" });
   config.addPassthroughCopy("src/assets");
   config.addPassthroughCopy("src/work/**/*.{png,jpg,gif}");
-  // merges directory-level data with template-specific data when keys clash
-  config.setDataDeepMerge(true);
 
   Object.entries(single).forEach((code) => config.addShortcode(...code));
-
   Object.entries(paired).forEach((code) => config.addPairedShortcode(...code));
 
   const md = markdownIt({
