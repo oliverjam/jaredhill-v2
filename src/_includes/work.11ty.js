019b87d6041fb857d4eaf50b64df946f02244311
oliverjam <oliverjamesphillips@gmail.com>
2022-11-16 18:18:56 +0000

    Add layout for portfolio work.
    It will render an info box at the top if an "info" object
    is passed in the frontmatter.
    
    Fix Adding primary info box #83

---
+++ src/_includes/work.11ty.js
@@ -0,0 +1,23 @@
+const html = require("../html");
+
+exports.data = {
+  layout: "base.11ty.js",
+};
+
+exports.render = ({ info, content }) => {
+  if (info) {
+    const box = html`
+      <dl class="WorkInfo pad-lg bg-200 leading-sm">
+        ${Object.entries(info).map(
+          ([key, val]) => html`
+            <dt class="fw-600 capitalize fz-md text-align:right">${key}</dt>
+            <dd>${val}</dd>
+          `
+        )}
+      </dl>
+    `;
+    console.log({ box });
+    return box + content;
+  }
+  return content;
+};
